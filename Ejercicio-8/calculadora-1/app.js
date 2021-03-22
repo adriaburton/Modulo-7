4c92270aad8c5cab8a58b1569317fab9963e6032
adriaburton <74770916+adriaburton@users.noreply.github.com>
2021-03-22 20:16:59 +0100

    Add files via upload

---
+++ Ejercicio-8/calculadora-1/app.js
@@ -0,0 +1,37 @@
+var repetir = "si";
+
+function sumar(num1, num2) {
+  alert("Resultado" + (num1 + num2));
+}
+function restar(num1, num2) {
+  alert("Resultado" + (num1 - num2));
+}
+function multiplicar(num1, num2) {
+  alert("Resultado" + num1 * num2);
+}
+function dividir(num1, num2) {
+  alert("Resultado" + num1 / num2);
+}
+do {
+  var operacion = prompt(
+    "elige operacion a realizar: sumar,restar,multiplicar,dividir"
+  );
+
+  var operador1 = parseInt(prompt("Introduce el primer numero"));
+
+  var operador2 = parseInt(prompt("Introduce el segundo numero"));
+
+  if (operacion == "sumar") {
+    sumar(operador1, operador2);
+  } else if (operacion == "restar") {
+    restar(operador1, operador2);
+  } else if (operacion == " multiplicar") {
+    multiplicar(operador1, operador2);
+  } else if (operacion == "dividir") {
+    dividir(operador1, operador2);
+  } else {
+    alert("Error, no se realizar esta opreración");
+  }
+
+  repetir = prompt("Quieres ejecutar otra operacion?");
+} while (repetir == "si");
